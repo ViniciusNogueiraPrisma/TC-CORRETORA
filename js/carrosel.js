5f9d509a2ef10c0e19704be7d5d92afe30539d21
vinicius nogueira <v.francisco@riprisma.com>
2024-04-27 18:13:09 -0300

    alt

--- js/carrosel.js
+++ js/carrosel.js
@@ -28,3 +28,26 @@ var swiper = new Swiper(".mySwiper-cards-home", {
     clickable: true,
   },
 });
+
+function initSwiper() {
+  console.log("Width: ", window.innerWidth); // Verificar a largura da tela
+  if (window.innerWidth < 992) {
+    console.log("Initializing Swiper"); // Verificar se o Swiper está sendo inicializado
+    var swiper = new Swiper(".swiper-cards-invest", {
+      loop: false,
+      slidesPerView: "auto",
+      // spaceBetween: 12,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+  }
+}
+
+initSwiper();
+
+window.addEventListener("resize", function () {
+  console.log("Window resized"); // Verificar se o evento de redimensionamento está sendo detectado
+  initSwiper();
+});
