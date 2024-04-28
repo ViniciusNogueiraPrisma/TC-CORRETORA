fe3752b71e036645b0f5c52fe0e7332254ea3afc
vinicius nogueira <v.francisco@riprisma.com>
2024-04-28 19:22:10 -0300

    alt

--- js/carrosel.js
+++ js/carrosel.js
@@ -30,13 +30,12 @@ var swiper = new Swiper(".mySwiper-cards-home", {
 });
 
 function initSwiper() {
-  console.log("Width: ", window.innerWidth); // Verificar a largura da tela
+  console.log("Width: ", window.innerWidth);
   if (window.innerWidth < 992) {
-    console.log("Initializing Swiper"); // Verificar se o Swiper está sendo inicializado
+    console.log("Initializing Swiper");
     var swiper = new Swiper(".swiper-cards-invest", {
       loop: false,
       slidesPerView: "auto",
-      // spaceBetween: 12,
       pagination: {
         el: ".swiper-pagination",
         clickable: true,
@@ -48,6 +47,31 @@ function initSwiper() {
 initSwiper();
 
 window.addEventListener("resize", function () {
-  console.log("Window resized"); // Verificar se o evento de redimensionamento está sendo detectado
+  console.log("Window resized");
   initSwiper();
+});
+
+///////////////////
+
+function initSwiperPerson() {
+  console.log("Width: ", window.innerWidth);
+  if (window.innerWidth < 992) {
+    console.log("Initializing Swiper");
+    var swiper = new Swiper(".swiper-person", {
+      loop: false,
+      spaceBetween: 24,
+      slidesPerView: "auto",
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+  }
+}
+
+initSwiperPerson();
+
+window.addEventListener("resize", function () {
+  console.log("Window resized");
+  initSwiperPerson();
 });
